a966d3dfda95203407d469d2729b74bbf3b597dc
Alexander Belovitsky <mirrexone@gmail.com>
2024-11-17 07:00:04 +0300

    Add Kafka Listener for reading from topic

---
+++ src/main/java/dev/mirrex/KafkaListeners.java
@@ -0,0 +1,16 @@
+package dev.mirrex;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+
+    @KafkaListener(
+            topics = "mirrexdev",
+            groupId = "groupId"
+    )
+    void listener(String data) {
+        System.out.println("Listener received: " + data);
+    }
+}
